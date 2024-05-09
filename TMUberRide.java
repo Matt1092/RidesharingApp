165a6ea42dae0b760bb4d01ccfce56dcfce8b788
mattdaboi <146141224+mattdaboi@users.noreply.github.com>
2024-05-09 16:16:36 -0400

    Update TMUberRide.java

--- TMUberRide.java
+++ TMUberRide.java
@@ -1,19 +1,28 @@
-// NAME: MATTHEW MOGA
-// STUDENT NUMBER: 501253529
-/*
+/**
  * 
- * This class simulates an ride service for a simple Uber app
+ * This class simulates an ride service for a simple Uber app. 
+ * A TMUberRide is-a TMUberService with some extra functionality.
  * 
- * A TMUberRide is-a TMUberService with some extra functionality
+ * @author Matthew Moga
+ * @version April 12, 2024
+ * 
  */
 public class TMUberRide extends TMUberService
 {
   private int numPassengers;
   private boolean requestedXL;
-  
   public static final String TYPENAME = "RIDE";
-  // All references to Driver object are removed
-  // Driver driver removed from argument to constructor 
+
+
+  /**
+   * This constructor initializes all instance variables in class.
+   * 
+   * @param from (String)
+   * @param to (String)
+   * @param user (User)
+   * @param distance (int)
+   * @param cost (double)
+   */
   public TMUberRide(String from, String to, User user, int distance, double cost)
   {
     super(from, to, user, distance, cost, TMUberRide.TYPENAME);
@@ -21,26 +30,56 @@ public class TMUberRide extends TMUberService
     numPassengers = 1;
   }
   
+
+  /**
+   * Accessor method returns service type.
+   * 
+   * @return String - Type name
+   */
   public String getServiceType()
   {
     return TYPENAME;
   }
 
+
+  /**
+   * Accessor method returns number of passengers.
+   * 
+   * @return int - number of passengers
+   */
   public int getNumPassengers()
   {
     return numPassengers;
   }
 
+
+  /**
+   * Mutator method sets number of passengers.
+   * 
+   * @param numPassengers (int)
+   */
   public void setNumPassengers(int numPassengers)
   {
     this.numPassengers = numPassengers;
   }
 
+
+  /**
+   * Method returns boolean variable (whether or not requestedXL).
+   * 
+   * @return boolean - true or false
+   */
   public boolean isRequestedXL()
   {
     return requestedXL;
   }
 
+
+  /**
+   * Mutator method sets requestedXL instance variable.
+   * 
+   * @param requestedXL (boolean)
+   */
   public void setRequestedXL(boolean requestedXL)
   {
     this.requestedXL = requestedXL;
