64f396bd7998f54bf25958511361b220bf02e07d
mattdaboi <146141224+mattdaboi@users.noreply.github.com>
2024-05-09 16:25:06 -0400

    Update TMUberRegistered.java

--- TMUberRegistered.java
+++ TMUberRegistered.java
@@ -1,43 +1,65 @@
-// NAME: MATTHEW MOGA
-// STUDENT NUMBER: 501253529
+// Import statements
 import java.util.ArrayList;
 import java.util.Scanner;
-
-// Added import statement for file reading
 import java.io.File;
-// Added import statement for exception handling
 import java.io.IOException;
 
+
+/**
+ * 
+ * This class registers all drivers and users through file input.
+ * Takes users and drivers from a file, reads file then adds them to ArrayList.
+ * 
+ * @author Matthew Moga
+ * @version April 12, 2024
+ * 
+ */
 public class TMUberRegistered {
     // These variables are used to generate user account and driver ids
     private static int firstUserAccountID = 900;
     private static int firstDriverId = 700;
 
-    // Generate a new user account id
+
+    /**
+     * Method that generates a new user account ID.
+     * 
+     * @param current (ArrayList<User>)
+     * 
+     * @return String - user account ID
+     */
     public static String generateUserAccountId(ArrayList<User> current) {
         return "" + firstUserAccountID + current.size();
     }
 
-    // Generate a new driver id
+
+    /**
+     * Method that generates a new driver ID.
+     * 
+     * @param current (ArrayList<Driver>)
+     * 
+     * @return String - driver ID
+     */
     public static String generateDriverId(ArrayList<Driver> current) {
         return "" + firstDriverId + current.size();
     }
 
-    // Database of Preregistered users
-    // In Assignment 2 these will be loaded from a file
-    // The test scripts and test outputs included with the skeleton code use these
-    // users and drivers below. You may want to work with these to test your code
-    // (i.e. check your output with the
-    // sample output provided).
-    public static ArrayList<User> loadPreregisteredUsers(String filename) throws IOException {
 
+    /**
+     * Loads users from a file. Reads through each line in file and creates User object.
+     * Adds each User object to ArrayList of users then returs this list of users.
+     * 
+     * @param filename (String)
+     * 
+     * @return ArrayList<User> - list of users that are pre-registered
+     * 
+     * @throws IOException
+     */
+    public static ArrayList<User> loadPreregisteredUsers(String filename) throws IOException {
         // Create array list of users
         ArrayList<User> users = new ArrayList<User>();
-
         // Create a file that takes in filename as file name
         File usersFile = new File(filename);
         Scanner scanner = new Scanner(usersFile);
-
         while (scanner.hasNextLine()) {
             // Name is the first line in file
             String name = scanner.nextLine();
@@ -53,17 +75,23 @@ public class TMUberRegistered {
         return users;
     }
 
-    // Database of Preregistered users
-    // In Assignment 2 these will be loaded from a file
-    public static ArrayList<Driver> loadPreregisteredDrivers(String filename) throws IOException {
 
+    /**
+     * Loads drivers from a file. Reads through each line in file and creates Driver object.
+     * Adds each Driver object to ArrayList of drivers then returns this list of drivers.
+     * 
+     * @param filename (String)
+     * 
+     * @return ArrayList<Driver> - list of drivers that are pre-registered
+     * 
+     * @throws IOException
+     */
+    public static ArrayList<Driver> loadPreregisteredDrivers(String filename) throws IOException {
         // Create array list of drivers
         ArrayList<Driver> drivers = new ArrayList<Driver>();
-
         // Create a file that takes in filename as file name
         File driversFile = new File(filename);
         Scanner scanner = new Scanner(driversFile);
-
         while (scanner.hasNextLine()) {
             // Name is the first line in file
             String name = scanner.nextLine();
